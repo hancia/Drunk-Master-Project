17f5bd6fc55f0aa3a2c73a12cdacfa3b5aed02fa
magicunico <julia.dziuba@student.put.poznan.pl>
2018-05-25 23:30:17 +0200

    teraz -nie uzyje niecenzuralych slow - zadziala

---
+++ cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/cube.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/cube.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/detailed_cube.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/detailed_cube.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/glm/glm/detail/dummy.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/glm/glm/detail/dummy.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/glm/glm/detail/glm.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/glm/glm/detail/glm.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/lodepng.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/lodepng.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/main_file.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/main_file.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/model.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/model.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/sphere.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/sphere.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/teapot.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/teapot.cpp.obj"
+  "C:/Users/julia/Downloads/en_szkielet03_win/en_szkielet03_win/torus.cpp" "C:/Users/julia/Downloads/en_szkielet03_win/cmake-build-debug/CMakeFiles/en_szkielet03_win.dir/en_szkielet03_win/torus.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
